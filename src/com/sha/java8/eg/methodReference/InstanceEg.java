756b339a73fbe59cb4723ed5a52367625d0e09d0
Shashwat Anand <shashwat.anand@gmail.com>
2015-05-31 00:39:19 +0530

    Uodated

--- src/com/sha/java8/eg/methodReference/InstanceEg.java
+++ src/com/sha/java8/eg/methodReference/InstanceEg.java
@@ -6,16 +6,26 @@ import java.util.List;
 
 import com.sha.java8.eg.model.Student;
 
-public class StaticEg {
+public class InstanceEg {
 	public static void main(String[] args) {
+		InstanceEg obj = new InstanceEg();
+		obj.sortItems();
+		
+	}
+	
+	public void sortItems() {
 		List<Student> students = new ArrayList<Student>();
 		students.add(new Student(60, "Shashwat"));
 		students.add(new Student(20, "Manish"));
 		students.add(new Student(80, "Sumit"));
 		students.add(new Student(40, "Santosh"));
 		
-		Collections.sort(students, Student :: compareIds);
+		Collections.sort(students, this :: compareIds);
 		students.forEach(student -> System.out.println(student));
 	}
-
+	
+	public int compareIds(Student s1, Student s2) {
+		Integer id1 = s1.getId();
+		return id1.compareTo(s2.getId());
+	}
 }
