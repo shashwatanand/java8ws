272a0628ad9992419cdf77fe14bc5b077a6bf5c4
Shashwat Anand <shashwat.anand@gmail.com>
2015-05-31 17:50:29 +0530

    Parallel Stream

---
+++ src/com/sha/java8/eg/stream/ParallelStream.java
@@ -0,0 +1,33 @@
+package com.sha.java8.eg.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import com.sha.java8.eg.model.Student;
+
+public class ParallelStream {
+	public static void main(String[] args) {
+		List<Student> students = new ArrayList<Student>();
+		students.add(new Student(60, "Shashwat"));
+		students.add(new Student(20, "Manish"));
+		students.add(new Student(80, "Sumit"));
+		students.add(new Student(40, "Santosh"));
+		
+		Predicate<Student> pred = (s) -> s.getId() > 40;
+		displayStudent(students, pred);
+	}
+
+	private static void displayStudent(List<Student> students,
+			Predicate<Student> pred) {
+		System.out.println("Students met the criteria are ....");		
+		/*students.stream()
+			.parallel()
+			.filter(pred)
+			.forEach(s ->  System.out.println(s.getStudentInfo()));*/
+		
+		students.parallelStream()
+			.filter(pred)
+			.forEach(s ->  System.out.println(s.getStudentInfo()));
+	}
+}
