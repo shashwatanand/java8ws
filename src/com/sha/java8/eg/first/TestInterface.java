2827220eef22e091d324b88130910b93f0ec5be6
Shashwat Anand <shashwat.anand@gmail.com>
2015-06-01 19:16:42 +0530

    deleted

--- src/com/sha/java8/eg/first/TestInterface.java
+++
@@ -1,6 +0,0 @@
-package com.sha.java8.eg.first;
-
-@FunctionalInterface
-public interface TestInterface {
-	void test();
-}
--- src/com/sha/java8/eg/first/TestInterfaceMain.java
+++
@@ -1,8 +0,0 @@
-package com.sha.java8.eg.first;
-
-public class TestInterfaceMain {
-	public static void main(String[] args) {
-		TestInterface tester = () -> System.out.println("Java 8 is working");
-		tester.test();
-	}
-}
