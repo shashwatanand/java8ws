61d38cddef01a6f06ef82b57b24f0273af221463
Shashwat Anand <shashwat.anand@gmail.com>
2015-06-06 22:22:24 +0530

    Modified Package Name

--- src/com/sha/java8/jjs_nashorn/FirstEg.java
+++ src/com/sha/java8/jjs_nashorn/FirstEg.java
@@ -1,9 +1,10 @@
-package com.sha.java8.jjs;
+package com.sha.java8.jjs_nashorn;
 
 import javax.script.ScriptEngineManager;
 
 public class FirstEg {
 	public static void main(String[] args) {
 		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		scriptEngineManager.getEngineByName("nashorn");
 	}
 }
