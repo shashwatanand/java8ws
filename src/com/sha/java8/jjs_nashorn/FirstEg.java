58c1243fd21bf3381239a4125b335c9203bdfb1f
Shashwat Anand <shashwat.anand@gmail.com>
2015-06-06 22:40:00 +0530

    Updated FirstEg

--- src/com/sha/java8/jjs_nashorn/FirstEg.java
+++ src/com/sha/java8/jjs_nashorn/FirstEg.java
@@ -1,10 +1,24 @@
 package com.sha.java8.jjs_nashorn;
 
+import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class FirstEg {
 	public static void main(String[] args) {
 		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-		scriptEngineManager.getEngineByName("nashorn");
+		ScriptEngine engine = scriptEngineManager.getEngineByName("nashorn");
+		
+		String script = "var greet = 'Hello'; "
+				+ "greet += ' Shashwat'; "
+				+ "greet;";
+		
+		Object output;
+		try {
+			output = engine.eval(script);
+			System.out.println(output);
+		} catch (Exception e) {
+			System.out.println("Javascript Exception");
+			e.printStackTrace();
+		}
 	}
 }
