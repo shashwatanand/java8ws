ddb352b922452193020410533ce4a10c9ad17d98
Shashwat Anand <shashwat.anand@gmail.com>
2015-06-06 22:19:22 +0530

    Added JJS

---
+++ src/com/sha/java8/jjs/FirstEg.java
@@ -0,0 +1,9 @@
+package com.sha.java8.jjs;
+
+import javax.script.ScriptEngineManager;
+
+public class FirstEg {
+	public static void main(String[] args) {
+		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+	}
+}
